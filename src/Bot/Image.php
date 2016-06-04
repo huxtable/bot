545f4e8d6823a67fc1e2c85d6d23f6924f26a2cb
Ashur Cabrera <ashur@panic.com>
2016-06-03 17:06:27 -0700

    no longer hardcoding font path 😰

--- src/Bot/Image.php
+++ src/Bot/Image.php
@@ -29,25 +29,20 @@ class Image
 	 * @param	string	$fontColor
 	 * @return	void
 	 */
-	public function addCaption( $text, $fontSize, $fontColor, $height, $shadowOffset=3 )
+	public function addCaption( $text, $fontSize, $fontPath, $fontColor, $height, $shadowOffset=3 )
 	{
-		//function generateCaption( $text, $fontSize, $height, $shadowOffset=3, $gravity='center' )
-
 		$dirTemp = $this->source->parent();
 
 		$fileBlack = $dirTemp->child( 'text-black.png' );
 		$fileColor = $dirTemp->child( 'text-color.png' );
 		$fileShadow = $dirTemp->child( 'text-shadow.png' );
 
-		echo $text . PHP_EOL;
-
 		$gravity = 'center';
-		$pathFont = '/Library/Fonts/CooperBlackLTPro.otf';
 		$heightShadow = $height - $shadowOffset;
 
-		exec( "convert -background \"#ffffff\" -fill \"#000000\" -font {$pathFont} -pointsize {$fontSize} -size 640x{$heightShadow} -gravity {$gravity} label:\"{$text}\" {$fileShadow}" );
-		exec( "convert -background \"#ffffff\" -fill \"#000000\" -font {$pathFont} -pointsize {$fontSize} -size 640x{$height} -gravity {$gravity} label:\"{$text}\" {$fileBlack}" );
-		exec( "convert -background \"#000000\" -fill \"#{$fontColor}\" -font {$pathFont} -pointsize {$fontSize} -size 640x{$height} -gravity {$gravity} label:\"{$text}\" {$fileColor}" );
+		exec( "convert -background \"#ffffff\" -fill \"#000000\" -font {$fontPath} -pointsize {$fontSize} -size 640x{$heightShadow} -gravity {$gravity} label:\"{$text}\" {$fileShadow}" );
+		exec( "convert -background \"#ffffff\" -fill \"#000000\" -font {$fontPath} -pointsize {$fontSize} -size 640x{$height} -gravity {$gravity} label:\"{$text}\" {$fileBlack}" );
+		exec( "convert -background \"#000000\" -fill \"#{$fontColor}\" -font {$fontPath} -pointsize {$fontSize} -size 640x{$height} -gravity {$gravity} label:\"{$text}\" {$fileColor}" );
 
 		exec( "composite -gravity south -compose multiply {$fileShadow} {$this->source} {$this->source}" );
 		exec( "composite -gravity south -compose multiply {$fileBlack} {$this->source} {$this->source}" );
